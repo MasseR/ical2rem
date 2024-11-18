6f7a547bcef76858bc7c56e836899c1c237998f8
Mats Rauhala <mats.rauhala@iki.fi>
2024-11-18 19:23:58 +0200

    Use the flake-utils

--- flake.nix
+++ flake.nix
@@ -2,27 +2,32 @@
 {
   description = "A very basic flake";
   inputs = {
+    flake-utils = { url = "github:numtide/flake-utils"; };
   };
 
-  outputs = { self, nixpkgs }: rec {
-    packages.x86_64-linux.default = packages.x86_64-linux.ical2rem;
-    packages.x86_64-linux.ical2rem = with nixpkgs.legacyPackages.x86_64-linux; stdenv.mkDerivation {
-      pname = "ical2rem";
-      version = "0.8";
-      src = ./.;
-      buildPhase = ''
-        chmod +x ical2rem.pl
-        patchShebangs ical2rem.pl
-      '';
-      installPhase = ''
-        mkdir -p $out/bin/
-        cp ical2rem.pl $out/bin/
-      '';
-      buildInputs = [perl];
-      nativeBuildInputs = [makeWrapper];
-      postFixup = ''
-        wrapProgram $out/bin/ical2rem.pl --prefix PERL5LIB : "${with perlPackages; makeFullPerlPath [iCalParser TimeDate]}"
-      '';
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
+      rec {
+        defaultPackage = packages.ical2rem;
+        packages.ical2rem = with nixpkgs.legacyPackages."${system}";
+          stdenv.mkDerivation {
+            pname = "ical2rem";
+            version = "0.8";
+            src = ./.;
+            buildPhase = ''
+            chmod +x ical2rem.pl
+            patchShebangs ical2rem.pl
+            '';
+            installPhase = ''
+            mkdir -p $out/bin/
+            cp ical2rem.pl $out/bin/
+            '';
+            buildInputs = [perl];
+            nativeBuildInputs = [makeWrapper];
+            postFixup = ''
+            wrapProgram $out/bin/ical2rem.pl --prefix PERL5LIB : "${with perlPackages; makeFullPerlPath [iCalParser TimeDate]}"
+            '';
+          };
+      }
+    );
 }
