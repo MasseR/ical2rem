279445abbb2d63d950f1037bc7533743afec2ad9
Mats Rauhala <mats.rauhala@iki.fi>
2024-11-18 18:58:30 +0200

    Add a flake.nix
    Flakes are a mechanism in nix for creating packages

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+  inputs = {
+  };
+
+  outputs = { self, nixpkgs }: rec {
+    packages.x86_64-linux.default = packages.x86_64-linux.ical2rem;
+    packages.x86_64-linux.ical2rem = with nixpkgs.legacyPackages.x86_64-linux; stdenv.mkDerivation {
+      pname = "ical2rem";
+      version = "0.8";
+      src = ./.;
+      buildPhase = ''
+        chmod +x ical2rem.pl
+        patchShebangs ical2rem.pl
+      '';
+      installPhase = ''
+        mkdir -p $out/bin/
+        cp ical2rem.pl $out/bin/
+      '';
+      buildInputs = [perl];
+      nativeBuildInputs = [makeWrapper];
+      postFixup = ''
+        wrapProgram $out/bin/ical2rem.pl --prefix PERL5LIB : "${with perlPackages; makeFullPerlPath [iCalParser TimeDate]}"
+      '';
+    };
+  };
+}
